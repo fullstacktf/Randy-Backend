ab653807596524c6f03b3285bfda25a5402f1517
Ruben Zagon <ruben.zagon@gmail.com>
2019-12-12 17:16:04 +0000

    Add instrucctions to handle the tasklist, and refactoring to use tasklist

---
+++ docs/Task_List.md
@@ -0,0 +1,13 @@
+## Comandos de la api para manejar la Task List
+
+### GET
+
+| url            | Descripción        |
+| -------------- | ------------------ |
+| /api/taskslist | Entrega las tareas |
+
+### POST
+
+| url       | Descripción                                                                                            |
+| --------- | ------------------------------------------------------------------------------------------------------ |
+| /api/task | Añade una nueva tarea que tenga en [**key** - `contain`] y en [**value** - `El contenido de la tarea`] |
--- src/testServer.js
+++ src/testServer.js
@@ -1,64 +1,66 @@
-const express = require('express'),
-  mongoose = require('mongoose'),
-  bodyParser = require('body-parser'),
-  cors = require('cors'),
+const express = require("express"),
+  mongoose = require("mongoose"),
+  bodyParser = require("body-parser"),
+  cors = require("cors"),
   app = express();
 
 //configure CORS
 app.use(cors());
 
 //configure app to use body-parser
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 8080;
 
-//const MONGO_URL = "172.17.0.3:27017"
-const MONGO_URL = "mongodb:27017"
+// Habilita esta línea para modo develop
+//const MONGO_URL = "172.17.0.2:27017";
+
+// Habilita esta línea para modo produccion
+const MONGO_URL = "mongodb:27017";
 
 mongoose.connect(`mongodb://${MONGO_URL}`, err => {
-  if (err)
-    throw err;
-  console.log('Connected to mongodb');
+  if (err) throw err;
+  console.log("Connected to mongodb");
 });
 
 //mongoDB service
-var Topic = require('./models/topics');
+const Task = require("./api/tasks_list/models/task.js");
 
 //Routes
-var router = express.Router();
+const router = express.Router();
 
-router.post('/topics', (req, res) => {
-  console.log('[POST] Topics');
+router.post("/task", (req, res) => {
+  console.log("[POST] Task");
 
-  var topic = new Topic();
-  topic.name = req.body.name;
+  const task = new Task();
+  task.contain = req.body.contain;
 
-  topic.save(err => {
-    if (err)
-      res.status(500).send(err);
+  task.save(err => {
+    if (err) res.status(500).send(err);
 
     res.status(201).json({
-      message: 'Topic created!'
+      message: "Task created!"
     });
   });
 });
 
-router.get('/topics', (req, res) => {
-  console.log('[GET] Topics');
+router.get("/taskslist", (req, res) => {
+  console.log("[GET] Task list");
 
-  Topic.find((err, topics) => {
-    if (err)
-      res.status(500).send(err);
+  Task.find((err, tasks) => {
+    if (err) res.status(500).send(err);
 
-    res.status(200).json(topics);
+    res.status(200).json(tasks);
   });
 });
 
 //all routes will be prefixed with /api
-app.use('/api', router);
+app.use("/api", router);
 
 //start the server
 app.listen(port, () => {
